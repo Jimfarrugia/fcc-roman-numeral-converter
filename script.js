63bc3bea7f8e85b52248e745f9163631eda89ec9
Jim <the.jim.farrugia@gmail.com>
2021-08-25 09:32:39 +1000

    identified issue and made comments

--- script.js
+++ script.js
@@ -22,20 +22,31 @@ function convertToRoman(num) {
   // loop through $keys from highest to lowest,
   // checking whether $input is >= the current key
   keys.map((key) => {
+    // ? console.log("checking", key);
     if (input >= +key) {
+      // ? console.log("found", key);
+
       // If true, append $result with the key's value (letter)
+
       result += numerals[key];
+      // ? console.log("appeneded", numerals[key], "to result");
+
       // and subtract the key (number) from $input
+
       input -= +key;
     }
   });
 
+  // ! Each key is checked only once.
+  // ! So, when 10 is found in 36 we are not checking for another 10
+  // ! instead, the next key (9) is checked for.
+
   return result;
 }
 
-console.log(convertToRoman(1));
-console.log(convertToRoman(10));
-console.log(convertToRoman(11));
-console.log(convertToRoman(15));
+// console.log(convertToRoman(1));
+// console.log(convertToRoman(10));
+// console.log(convertToRoman(11));
+// console.log(convertToRoman(15));
 console.log(convertToRoman(36));
-console.log(convertToRoman(1001));
+// console.log(convertToRoman(1001));
