e17545eb346c5919e4abfbcb3be9030d66785925
Jim <the.jim.farrugia@gmail.com>
2021-08-25 12:53:14 +1000

    fix typo in numerals object

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function convertToRoman(num) {
     40: "XL",
     50: "L",
     90: "XC",
-    100: "X",
+    100: "C",
     400: "CD",
     500: "D",
     900: "CM",
@@ -19,41 +19,24 @@ function convertToRoman(num) {
   let input = num;
   let result = "";
 
-  console.log("converting", input);
-
   // loop through $keys from highest to lowest,
   // checking whether $input is >= the current key
   for (let i = 0; i < keys.length; i++) {
     const key = +keys[i];
-    console.log("checking", key);
-
     if (input >= key) {
-      console.log("found", key);
-
       // If true, append $result with the key's value (letter)
       result += numerals[key];
-      console.log("appended", numerals[key], "to result.");
-
       // subtract the key (number) from $input
-      console.log("input (", input, ") -", key);
       input -= key;
-      console.log("input =", input);
-
       // decrement iterator so we can check for the current key again
       i--;
     }
-    if (input < 1) break;
+    if (input < 1) break; // break out if input reached 0
   }
 
-  console.log("Done.");
   return result || 0;
 }
 
-// ? Failed tests:
-// ! convertToRoman(649) should return the string DCXLIX
-// ! convertToRoman(798) should return the string DCCXCVIII
-// ! convertToRoman(891) should return the string DCCCXCI
-
-console.log(convertToRoman(649));
-// console.log(convertToRoman(798));
-// console.log(convertToRoman(891));
+console.log(convertToRoman(649)); // DCXLIX
+console.log(convertToRoman(798)); // DCCXCVIII
+console.log(convertToRoman(891)); // DCCCXCI
